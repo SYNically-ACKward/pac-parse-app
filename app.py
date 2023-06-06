8499cd51032c43373c0fea6ac07101ae96475ac7
SYNically-ACKward <ryan@thenetworkguy.org>
2023-06-06 15:53:25 -0400

    update routing logic to allow for repeat query

--- app.py
+++ app.py
@@ -3,27 +3,42 @@ import pacparser
 
 app = Flask(__name__)
 
+# Initialize pacparser
+pacparser.init()
+
 # Home page
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        # Get the uploaded PAC file
-        uploaded_file = request.files['file']
-        pac_file_content = uploaded_file.read().decode('utf-8')
+        # Check if the user submitted a new PAC file
+        if 'file' in request.files:
+            # Get the uploaded PAC file
+            uploaded_file = request.files['file']
+            pac_file_content = uploaded_file.read().decode('utf-8')
+
+            # Parse the new PAC file
+            pacparser.parse_pac_string(pac_file_content)
 
         # Get the user-provided URL
         url_to_test = request.form['url']
 
-        # Initialize pacparser and parse the PAC file
-        pacparser.init()
-        pacparser.parse_pac_string(pac_file_content)
-
         # Find the proxy configuration for the URL
         proxy_config = pacparser.find_proxy(url_to_test)
 
-        return render_template('result.html', proxy_config=proxy_config)
-    
+        return render_template('result.html', proxy_config=proxy_config, query=url_to_test)
+
     return render_template('index.html')
 
+# Results page
+@app.route('/result', methods=['POST'])
+def result():
+    # Get the user-provided URL
+    url_to_test = request.form['query']
+
+    # Find the proxy configuration for the URL
+    proxy_config = pacparser.find_proxy(url_to_test)
+
+    return render_template('result.html', proxy_config=proxy_config, query=url_to_test)
+
 if __name__ == '__main__':
-    app.run(host='localhost',debug=True)
+    app.run(host='localhost', debug=True)
